dc820b53e3e33c98de41e6d2e82ba79b4540333a
MiguelSilva1999 <misos1999@gmail.com>
2021-01-05 15:39:33 +0000

    Login e Registo iniciado

--- app/src/main/java/com/example/revampedvision/LoginUti.java
+++ app/src/main/java/com/example/revampedvision/LoginUti.java
@@ -1,5 +1,6 @@
 package com.example.revampedvision;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,9 +10,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginUti extends AppCompatActivity {
     EditText et_mail, et_password;
     Button bt_login;
+    private FirebaseAuth mAuth;
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,11 +34,40 @@ public class LoginUti extends AppCompatActivity {
         et_mail = findViewById(R.id.et_login_email);
         et_password = findViewById(R.id.et_login_password);
         bt_login = findViewById(R.id.bt_login_entrar);
+        mAuth = FirebaseAuth.getInstance();
         bt_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(LoginUti.this,Menu.class);
-                startActivity(i);
+                String email = et_mail.getText().toString();
+                String password = et_password.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginUti.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Intent i = new Intent(LoginUti.this,Menu.class);
+                                    startActivity(i);
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+
+                                    Toast.makeText(LoginUti.this, "Autenticação Errada",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+
+
+                            }
+                        });
+
+
+
+
+
             }
         });
 
--- app/src/main/java/com/example/revampedvision/Reg.java
+++ app/src/main/java/com/example/revampedvision/Reg.java
@@ -1,5 +1,6 @@
 package com.example.revampedvision;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,8 +10,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Reg extends AppCompatActivity {
     private ViewHolder mViewHolder = new ViewHolder();
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,10 +27,46 @@ public class Reg extends AppCompatActivity {
         mViewHolder.et_password=findViewById(R.id.et_registo_password);
         mViewHolder.et_passwordrep =findViewById(R.id.et_registo_password1);
         mViewHolder.bt_registar=findViewById(R.id.bt_registo_registar);
+        mAuth = FirebaseAuth.getInstance();
         mViewHolder.bt_registar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                String email = mViewHolder.et_email.getText().toString();
+                String password =  mViewHolder.et_password.getText().toString();
+                String password1 =  mViewHolder.et_passwordrep.getText().toString();
+                if(!password.equals(password1))
+                {
+                    Toast.makeText(Reg.this, "Palavra-passe não coincide", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Reg.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(Reg.this, "Utilizador Registado com Sucesso",
+                                            Toast.LENGTH_SHORT).show();
+                                    finish();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Reg.this, "Registo Invalido.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+
+                                // ...
+                            }
+                        });
+
+
             }
         });
 
