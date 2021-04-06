a1cb173289c2e0c730fb1cb0500de02cccc7bc88
MiguelSilva1999 <misos1999@gmail.com>
2021-04-06 16:39:31 +0100

    app_sem_not

--- app/src/main/java/com/example/revampedvision/iluminacao.java
+++ app/src/main/java/com/example/revampedvision/iluminacao.java
@@ -1,21 +1,14 @@
 package com.example.revampedvision;
 
-import androidx.annotation.NonNull;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 
 public class iluminacao extends AppCompatActivity {
@@ -39,40 +32,14 @@ public class iluminacao extends AppCompatActivity {
             public void onClick(View v) {
                 if (switchCompat.isChecked()) {
                     imageView.setImageDrawable(getResources().getDrawable(R.drawable.light_on));
-                    databaseReference.addValueEventListener(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            String value = snapshot.child("led_STATUS").getValue(String.class);
-                            Toast.makeText(iluminacao.this, value, Toast.LENGTH_SHORT).show();
-                           if (value.equals("ON")) {
-
-
-                                dados.setLED_STATUS("ON");
-
-                                databaseReference.setValue(dados);
-
-
-                            }
-                           else if (value.equals("OFF")){
-                               imageView.setImageDrawable(getResources().getDrawable(R.drawable.light_off));
-                               dados.setLED_STATUS("OFF");
-                               databaseReference.setValue(dados);}
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-
-                        }
-
-
-
-                    });
-
-                    }
+                    databaseReference.child("led_STATUS").setValue("ON");
+                } else {
+                    imageView.setImageDrawable(getResources().getDrawable(R.drawable.light_off));
+                    databaseReference.child("led_STATUS").setValue("OFF");
                 }
 
-
-
+                  
+            }
         });
     }
 }
--- app/src/main/java/com/example/revampedvision/led.java
+++ app/src/main/java/com/example/revampedvision/led.java
@@ -3,20 +3,15 @@ package com.example.revampedvision;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class led extends AppCompatActivity implements View.OnClickListener{
-    Button bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9, bt10, bt11, bt12;
-    String Cor1 = "branco";
+    Button bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9, bt10, bt11, bt12,bt13,bt14,bt15,bt16;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +41,14 @@ public class led extends AppCompatActivity implements View.OnClickListener{
         bt11.setOnClickListener(this);
         bt12 = findViewById(R.id.bt12);
         bt12.setOnClickListener(this);
+        bt13 = findViewById(R.id.bt13);
+        bt13.setOnClickListener(this);
+        bt14 = findViewById(R.id.bt14);
+        bt14.setOnClickListener(this);
+        bt15 = findViewById(R.id.bt15);
+        bt15.setOnClickListener(this);
+        bt16 = findViewById(R.id.bt16);
+        bt16.setOnClickListener(this);
     }
 
     @Override
@@ -57,26 +60,32 @@ public class led extends AppCompatActivity implements View.OnClickListener{
         else if (v.getId() == R.id.bt2)
             databaseReference.child("Cor").setValue("amarelo");
         else if (v.getId() == R.id.bt3)
-            databaseReference.child("Cor").setValue("amarelo");
+            databaseReference.child("Cor").setValue("amarelotorrado");
         else if (v.getId() == R.id.bt4)
-            databaseReference.child("Cor").setValue("amarelo");
-        else if (v.getId() == R.id.bt4)
-            databaseReference.child("Cor").setValue("amarelo");
+            databaseReference.child("Cor").setValue("laranja");
         else if (v.getId() == R.id.bt5)
-            databaseReference.child("Cor").setValue("amarelo");
+            databaseReference.child("Cor").setValue("laranjaescuro");
         else if (v.getId() == R.id.bt6)
-            databaseReference.child("Cor").setValue("amarelo");
+            databaseReference.child("Cor").setValue("vermelho");
         else if (v.getId() == R.id.bt7)
-            databaseReference.child("Cor").setValue("amarelo");
+            databaseReference.child("Cor").setValue("rosachoque");
         else if (v.getId() == R.id.bt8)
-            databaseReference.child("Cor").setValue("amarelo");
+            databaseReference.child("Cor").setValue("magenta");
         else if (v.getId() == R.id.bt9)
-            databaseReference.child("Cor").setValue("amarelo");
+            databaseReference.child("Cor").setValue("rosa");
         else if (v.getId() == R.id.bt10)
-            databaseReference.child("Cor").setValue("amarelo");
+            databaseReference.child("Cor").setValue("lilas");
         else if (v.getId() == R.id.bt11)
-            databaseReference.child("Cor").setValue("amarelo");
+            databaseReference.child("Cor").setValue("roxo");
         else if (v.getId() == R.id.bt12)
-            databaseReference.child("Cor").setValue("amarelo");
+            databaseReference.child("Cor").setValue("azulescuro");
+        else if (v.getId() == R.id.bt13)
+            databaseReference.child("Cor").setValue("azul");
+        else if (v.getId() == R.id.bt14)
+            databaseReference.child("Cor").setValue("ciano");
+        else if (v.getId() == R.id.bt15)
+            databaseReference.child("Cor").setValue("verde");
+        else if (v.getId() == R.id.bt16)
+            databaseReference.child("Cor").setValue("verdeescuro");
     }
 }
